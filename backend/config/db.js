b490aaf1619ceece826a852aa894a6a1fe8a14f6
Sagar Rana <sgamilcom77@gmail.com>
2024-11-11 14:55:16 +0530

    changed in db connection code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-const mongoose=require('mongoose');
-const connectDB=async()=>{
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error('Error connecting to MongoDB:', error.message);
         process.exit(1);
     }
-}
+};
+
 module.exports = connectDB;
